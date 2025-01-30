b21002716a4fcac1ece256d439494b4062f25fa8
amiof <arm131313@gmail.com>
2025-01-30 16:54:28 +0330

    first commit

---
+++ electron/main.ts
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+let mainWindow: BrowserWindow | null;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadURL("http://localhost:3000");
+  // mainWindow.loadURL(`file://${path.join(__dirname, '../dist/index.html')}`);
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
