2734584a4b3e78635ab65a06831a459d64318d62
amiof <arm131313@gmail.com>
2025-02-07 21:02:48 +0330

    finaly can build project

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./react"),
-    },
+  base: './', // Ensures assets are correctly referenced
+  build: {
+    outDir: 'dist',
   },
   server: {
     port: 3000,
