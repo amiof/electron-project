492c04c6775eadb0636cb9aef9556e686fc7e55c
amiof <arm131313@gmail.com>
2025-02-10 11:55:06 +0330

    install scss to project

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
+import './App.scss'
 
 function App() {
 
     return (
         <>
-            <div className={"text-yellow-400"}>
+            <div className={"bg-amber-700"}> this is tailwindcss</div>
+            <div className={"text text-yellow-400"}>
                 this is a test
             </div>
         </>
