fa570fb33c6aa6e7052ba7f0297c86b4d1894ab4
amiof <arm131313@gmail.com>
2025-02-10 21:14:09 +0330

    install chakra ui to project

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,15 +1,19 @@
-import './App.scss'
+import "./App.scss";
+import { Button, HStack } from "@chakra-ui/react";
 
 function App() {
-
-    return (
-        <>
-            <div className={"bg-amber-700"}> this is tailwindcss</div>
-            <div className={"text text-yellow-400"}>
-                this is a test
-            </div>
-        </>
-    )
+  return (
+    <div>
+      <HStack>
+        <Button size={"2xl"} style={{ color: "red" }} css={{ bg: "green.500" }}>
+          Click me
+        </Button>
+        <Button disabled={true}>Click me</Button>
+      </HStack>
+      <div className={"bg-amber-700"}> this is tailwindcss</div>
+      <div className={"text text-yellow-400"}>this is a test</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
